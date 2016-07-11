0e7b5fb5d425da64695cab499a3a122fc41fb924
Jimmy Madon <admin@jimmymadon.com>
2016-07-11 16:17:02 +0100

    Added support for darwin to query processor

--- src/main/java/com/develogical/app/QueryProcessor.java
+++ src/main/java/com/develogical/app/QueryProcessor.java
@@ -8,6 +8,13 @@ public class QueryProcessor {
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
         }
+
+        if (query.toLowerCase().contains("darwin")) {
+            return "Charles Robert Darwin, FRS FRGS FLS FZS, (12 February " +
+                    "1809 - 19 April 1882) was an English naturalist and geologist, best " +
+                    "known for his contributions to the science of evolution.";
+        }
+
         return "";
     }
 }
